d43a0fd8d6fb794b56f0b5b6cacc87205ae1e388
Kati Ealey <kati.majorealey@gmail.com>
2023-10-16 10:15:36 -0400

    started creating react routes and created the root route to build off of.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Root from "./routes/route";
+import ErrorPage from "./error-page";
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
