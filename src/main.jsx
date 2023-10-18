5575684f56d1558216c556db817b0258d5f10e6f
Kati Ealey <kati.majorealey@gmail.com>
2023-10-17 21:33:48 -0400

    this updated PR should include modified files resolving the import error, file structure, and package-lock.json and package.json updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Root from "./routes/route";
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+import Root from "./routes/Root.jsx";
 import ErrorPage from "./error-page";
 import './index.css'
 
@@ -15,6 +16,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* does <App /> need taken out altogether or can it stay where it is for now? */}
+    <App /> 
   </React.StrictMode>,
 )
