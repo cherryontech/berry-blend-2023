e0e9411a82c2fe514a68f7f931b1d1249ef1c1b1
Kati Ealey <kati.majorealey@gmail.com>
2023-10-24 22:00:38 -0400

    IT's ALIVE! This PR addresses  setting up the React Router and beginning to build the home page in it's entirety

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
 
-import { Routes, Route } from 'react-router-dom';
-import Home from './routes/Home';
-import ErrorPage from './routes/ErrorPage';
-import Resources from './routes/Resources';
-import Questions from './routes/Questions';
-import Recs from './routes/Recs';
 import NavBar from './components/NavBar';
+import RoutePaths from './components/RoutePaths'
+// import Home from './routes/Home'
 import "./index.css";
 
 const App = () => {
  return (
    <div>
     <NavBar />
-       <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/Resources" element={<Resources />} />
-          <Route path="/Questions" element={<Questions />} />
-          <Route path="/Recs" element={<Recs />} />
-          <Route path='*' element={<ErrorPage />}/>
-       </Routes>
+   <RoutePaths />
    </div>
     
     
