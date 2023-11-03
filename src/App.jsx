ba2110e9318101b0aaee6b7d4841069c3bf3397b
Ann Nguyen <ann.nguyen.codes@gmail.com>
2023-11-03 10:45:12 -0400

    test css import

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import NavBar from './components/NavBar';
 import RoutePaths from './components/RoutePaths';
 // import Home from './routes/Home'
 
+import "./App.css"
+
 const App = () => {
   return (
     <div>
+      <h1>askjhdjakshdah</h1>
       <NavBar />
       <RoutePaths />
     </div>
