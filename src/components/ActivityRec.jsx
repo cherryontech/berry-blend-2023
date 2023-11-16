6d23e94907f2df95828b82058a5030bb0da06e9d
Kati Ealey <kati.majorealey@gmail.com>
2023-11-15 22:45:20 -0500

    this push adds in the activity recommendation componenents for after the question answers are submitted. Still need to work out some styling things, but it's something to work with for now.

---
+++ src/components/ActivityRec.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import AddToCalendar from '../components/buttons/AddToCalendar';
+
+export default function ActivityRec() {
+  return (
+    <div className="activity--container">
+      <section className="activity-rec-box">
+        <div className="image--container">
+          <img alt="activity-recommendation based on burnout score" src="#" />
+        </div>
+        <div className="activity-info-container">
+          <h3 className="activity--title">Activity Name</h3>
+          <p className="activity--description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem accusantium tempora voluptatum
+            pariatur, omnis dignissimos quaerat vero neque culpa deserunt quod cumque mollitia explicabo? Aperiam
+            commodi expedita molestiae eveniet architecto.
+          </p>
+          <h5 className="activity--duration">Duration:</h5>
+          <h5 className="activity--equipment">Equipment Needed: </h5>
+          <AddToCalendar />
+        </div>
+      </section>
+    </div>
+  );
+}
