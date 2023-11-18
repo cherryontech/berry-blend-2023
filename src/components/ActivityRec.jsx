5c491692c37aec3e12f98e85ca6ef58aeff6f48b
Kati Ealey <kati.majorealey@gmail.com>
2023-11-18 10:24:57 -0500

    with this branch I am adding in some real information to help make this look more relatistic. I notice there is some styling that I need to work out, but wanted to share a more live version of this. more to come.

--- src/components/ActivityRec.jsx
+++ src/components/ActivityRec.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import AddToCalendar from '../components/buttons/AddToCalendar';
 
-export default function ActivityRec() {
+export default function ActivityRec(props) {
   return (
     <div className="activity--container">
       <section className="activity-rec-box">
         <div className="image--container">
-          <img alt="" src="#" />
+          <img alt="" src={props.image} className="activity--image" />
         </div>
         <div className="activity-info-container">
-          <h3 className="activity--title">Activity Name</h3>
-          <p className="activity--description">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem accusantium tempora voluptatum
-            pariatur, omnis dignissimos quaerat vero neque culpa deserunt quod cumque mollitia explicabo? Aperiam
-            commodi expedita molestiae eveniet architecto.
-          </p>
-          <h5 className="activity--duration">Duration:</h5>
-          <h5 className="activity--equipment">Equipment Needed: </h5>
+          <h3 className="activity--title">{props.activity}</h3>
+          <p className="activity--description">{props.activityDesc}</p>
+          <h5 className="activity--duration">Duration: {props.duration}</h5>
+          <h5 className="activity--equipment">Equipment Needed: {props.equipment} </h5>
           <AddToCalendar />
         </div>
       </section>
--- src/routes/Recs.jsx
+++ src/routes/Recs.jsx
@@ -9,9 +9,27 @@ export default function Recommendations() {
         <h1>Your Score </h1>
         <p>Based on your score you may be experiencing... Here are your recommended activities.</p>
       </header>
-      <ActivityRec />
-      <ActivityRec />
-      <ActivityRec />
+      <ActivityRec
+        image="src/assets/walkingActivity.jpeg"
+        activity="Walking"
+        activityDesc="Take a moment to step outside and breathe the fresh air. Walking by yourself is a great opportunity to be mindful of the moment and disconnect from work."
+        duration="30 minutes"
+        equipment="none needed"
+      />
+      <ActivityRec
+        image="src/assets/yogaActivity.jpeg"
+        activity="Yoga Class"
+        activityDesc="Sign up for a Yoga class in your community. It provides an opportunity to be in community with others and to focus on your health. "
+        duration="1 hour"
+        equipment="none needed"
+      />
+      <ActivityRec
+        image="src/assets/journaling.jpeg"
+        activity="Journaling"
+        activityDesc="Find a comfortable spot to write and dump all of your thoughts and feelings on a piece of paper."
+        duration="30 minutes"
+        equipment="none needed"
+      />
     </div>
   );
 }
