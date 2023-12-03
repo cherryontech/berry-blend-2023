93c99c3feeb84f72e2ee7df67e5e6e3397179f7e
Kati Ealey <kati.majorealey@gmail.com>
2023-12-02 22:48:27 -0500

    updates to the last Recs page PR

--- src/components/buttons/AllResourcesButton.jsx
+++ src/components/buttons/AllResourcesButton.jsx
@@ -1,4 +1,4 @@
-export default function ResourcesButton() {
+export default function AllResourcesButton() {
   return (
     <button className="resources--button" onClick={(event) => (window.location.href = '/Resources')}>
       Explore All Resources
