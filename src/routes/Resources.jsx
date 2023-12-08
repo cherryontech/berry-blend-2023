7a4348265e3872f0f97e7cb86dcc654becf4bfd7
Joelle Bueno <tihardy@baypath.edu>
2023-12-08 11:20:50 -0500

    this PR sets up the format and content for the resources page

--- src/routes/Resources.jsx
+++ src/routes/Resources.jsx
@@ -1,5 +1,39 @@
-// import React from 'react';
+import React from 'react';
+import '../Resources.css';
+import { recommendationData } from '../static/recommendationData';
 
-const Resources = () => {return (<h1>Resources Page </h1>)} 
+const Resources = () => {
+  const listedItems = recommendationData.map((item) => (
+    <>
+      <p>{item.activity}</p>
+    </>
+  ));
+  const listedEnergy = recommendationData.map((item) => (
+    <>
+      <p>{item.energyRequired}</p>
+    </>
+  ));
+
+  return (
+    <div className="resources--container">
+      <div className="res--text">
+        <h1> Our Resources </h1>
+        <p>To access a list of all activities, look at this page</p>
+      </div>
+
+      <div class="row">
+        {' '}
+        <div class="column">
+          <h2 className="sub-head-left">Activity</h2>
+          <div> {listedItems}</div>
+        </div>
+        <div class="column">
+          <h2 className="sub-head-right">Energy level</h2>
+          <div> {listedEnergy}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Resources;
