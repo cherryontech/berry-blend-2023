45e967bd8179697630a50d84e8a4f6d73560d2a4
Ann Nguyen <ann.nguyen.codes@gmail.com>
2023-12-09 10:26:17 -0500

    fix logo and other

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,13 @@
 import { NavLink } from 'react-router-dom';
+import blissfulLogo from '../assets/blissfulLogo.jpg';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
 import '../App.css';
 
 const NavBar = () => {
   return (
-    <nav class="nav--container">
-      <img className="nav--logo" src="src/assets/blissfulLogo.jpg" alt="Blissful Website Logo with a cloud above it" />
+    <nav className="nav--container">
+      <img className="nav--logo" src={blissfulLogo} alt="Blissful Website Logo with a cloud above it" />
       <ul className="nav--links">
         <NavLink to="/" className="navLink--style">
           <li>Home</li>
